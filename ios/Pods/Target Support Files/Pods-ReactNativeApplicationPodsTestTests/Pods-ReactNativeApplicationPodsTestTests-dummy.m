e44ba95528fce078fd2bacde9e64ce7a2fbb6758
Evgenii Khramkov <evkhramk@microsoft.com>
2019-04-02 17:39:43 +0300

    add pods

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeApplicationPodsTestTests/Pods-ReactNativeApplicationPodsTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeApplicationPodsTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeApplicationPodsTestTests
+@end
